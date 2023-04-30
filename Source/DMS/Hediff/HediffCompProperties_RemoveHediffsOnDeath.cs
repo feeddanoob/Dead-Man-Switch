f71a93aa24078bc4c14fc31a333f7d6801cb64ff
Aoba <45892714+AobaKuma@users.noreply.github.com>
2023-05-01 07:23:14 +0800

    更新20230501
    DMS機控中樞與排異反應Hediff
    生產建築與材料
    科技樹
    武器
    部分機械體的身體結構與零件

---
+++ Source/DMS/Hediff/HediffCompProperties_RemoveHediffsOnDeath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace DMS
+{
+    public class HediffCompProperties_RemoveHediffsOnDeath : HediffCompProperties
+    {
+        public HediffCompProperties_RemoveHediffsOnDeath()
+        {
+            this.compClass = typeof(HediffComp_RemoveHediffsOnDeath);
+        }
+        public List<HediffDefWtihSeverity> hediffs;
+    }
+    //在死亡後添加或移除Hediff
+    public class HediffComp_RemoveHediffsOnDeath : HediffComp
+    {
+        private HediffCompProperties_RemoveHediffsOnDeath Props
+        {
+            get
+            {
+                return (HediffCompProperties_RemoveHediffsOnDeath)this.props;
+            }
+        }
+        public override void Notify_PawnDied()
+        {
+            base.Notify_PawnDied();
+            OnDeathHediffOperation(base.Pawn);
+        }
+        private void OnDeathHediffOperation(Pawn pawn)
+        {
+            for (int i = 0; i < this.Props.hediffs.Count; i++)
+            {
+                if (pawn.health.hediffSet.HasHediff(Props.hediffs[i].hediffDef))
+                {
+                    pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(Props.hediffs[i].hediffDef));
+                }
+            }
+        }
+    }
+}
---
+++ Source/DMS/Hediff/HediffComp_GiveHediffWtihSeverity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace DMS
+{
+    public class HediffDefWtihSeverity
+	{
+		public HediffDef hediffDef;
+		public float severity = 0;
+		public bool isAdd = true;
+	}
+    public class HediffCompProperties_GiveHediffWtihSeverity : HediffCompProperties
+	{
+		public HediffCompProperties_GiveHediffWtihSeverity()
+		{
+			this.compClass = typeof(HediffComp_GiveHediffWtihSeverity);
+		}
+		public List<HediffDefWtihSeverity> hediffDefAddSeverity;
+		public bool skipIfAlreadyExists = true;
+	}
+
+
+    public class HediffComp_GiveHediffWtihSeverity : HediffComp
+	{
+		private HediffCompProperties_GiveHediffWtihSeverity Props
+		{
+			get
+			{
+				return (HediffCompProperties_GiveHediffWtihSeverity)this.props;
+			}
+		}
+		//public override void CompPostPostRemoved()
+		//{
+		//	HediffOperation(this.parent.pawn);
+		//}
+        public override void CompPostMake()
+        {
+            HediffOperation(this.parent.pawn);
+            base.CompPostMake();
+        }
+        private void HediffOperation(Pawn pawn)
+        {
+			for (int i = 0; i < this.Props.hediffDefAddSeverity.Count; i++)
+			{
+				if (this.Props.skipIfAlreadyExists && pawn.health.hediffSet.HasHediff(this.Props.hediffDefAddSeverity[i].hediffDef, false))
+					continue;
+
+				Hediff hediff = HediffMaker.MakeHediff(this.Props.hediffDefAddSeverity[i].hediffDef, this.parent.pawn);
+				hediff.Severity = this.Props.hediffDefAddSeverity[i].severity;
+
+                if (this.Props.hediffDefAddSeverity[i].isAdd)
+                {
+                    pawn.health.AddHediff(hediff, null, null, null);
+                }
+                else
+                {
+                    pawn.health.RemoveHediff(hediff);
+                }
+            }
+		}
+	}
+}
