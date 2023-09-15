98e4d6445af7ce63204f806518b5b632821f9c16
Aoba <45892714+AobaKuma@users.noreply.github.com>
2023-09-15 23:27:38 +0800

    可用於Machine的武器替換使用
    ThingClass要改成DMS.WeaponUsableMachine

--- Source/DMS/Thing/WeaponUsableMech.cs
+++ Source/DMS/Thing/WeaponUsableMech.cs
@@ -3,6 +3,8 @@ using Verse;
 using RimWorld;
 using UnityEngine;
 using Verse.AI;
+using VFEMech;
+using VFE.Mechanoids;
 
 namespace DMS
 {
@@ -13,6 +15,37 @@ namespace DMS
         public bool EnableTechLevelFilter = false;
         public List<string> UsableTechLevels = new List<string>();
     }
+
+    //給VFEM機械使用的
+    public class WeaponUsableMachine : WeaponUsableMech
+    {
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            mindState.lastJobTag = JobTag.Idle;
+            guest = new Pawn_GuestTracker(this);
+            if (drafter == null && this.TryGetComp<CompMachine>().Props.violent)
+            {
+                drafter = new Pawn_DraftController(this);
+            }
+
+            if (base.Faction == Faction.OfPlayer && base.Name == null)
+            {
+                base.Name = PawnBioAndNameGenerator.GeneratePawnName(this, NameStyle.Numeric);
+            }
+        }
+
+        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.PostApplyDamage(dinfo, totalDamageDealt);
+            if (!health.Dead && (!health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) || !health.capacities.CapableOf(PawnCapacityDefOf.Moving)))
+            {
+                Kill(dinfo);
+            }
+        }
+    }
+
+    //一般情況的
     public class WeaponUsableMech : Pawn
     {
         public MechWeaponExtension MechWeapon { get; private set; }
@@ -89,12 +122,6 @@ namespace DMS
                 this.Drawer?.renderer?.graphics.SetAllGraphicsDirty();
             }
         }
-
-        public override void Draw()
-        {
-            base.Draw();
-        }
-
         public void Equip(ThingWithComps equipment)
         {
             equipment.SetForbidden(false);
