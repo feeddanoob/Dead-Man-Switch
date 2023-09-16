cb5b83cc5f670a80bfb0315953ef670758a0d130
Aoba <45892714+AobaKuma@users.noreply.github.com>
2023-09-17 01:31:13 +0800

    更新測試

---
+++ Source/DMS/Thing/WeaponUsableMachine.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+using RimWorld;
+using Verse.AI;
+using VFE.Mechanoids;
+
+namespace DMS
+{
+    //給VFEM機械使用的
+    public class WeaponUsableMachine : WeaponUsableMech
+    {
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            mindState.lastJobTag = JobTag.Idle;
+            guest = new Pawn_GuestTracker(this);
+            if (drafter == null && this.TryGetComp<CompMachine>().Props.violent)
+            {
+                drafter = new Pawn_DraftController(this);
+            }
+
+            if (base.Faction == Faction.OfPlayer && base.Name == null)
+            {
+                base.Name = PawnBioAndNameGenerator.GeneratePawnName(this, NameStyle.Numeric);
+            }
+        }
+
+        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.PostApplyDamage(dinfo, totalDamageDealt);
+            if (!health.Dead && (!health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) || !health.capacities.CapableOf(PawnCapacityDefOf.Moving)))
+            {
+                Kill(dinfo);
+            }
+        }
+    }
+}
--- Source/DMS/Thing/WeaponUsableMech.cs
+++ Source/DMS/Thing/WeaponUsableMech.cs
@@ -4,7 +4,6 @@ using RimWorld;
 using UnityEngine;
 using Verse.AI;
 using VFEMech;
-using VFE.Mechanoids;
 
 namespace DMS
 {
@@ -14,35 +13,6 @@ namespace DMS
         public List<string> UsableWeaponTags = new List<string>();
         public bool EnableTechLevelFilter = false;
         public List<string> UsableTechLevels = new List<string>();
-    }
-
-    //給VFEM機械使用的
-    public class WeaponUsableMachine : WeaponUsableMech
-    {
-        public override void SpawnSetup(Map map, bool respawningAfterLoad)
-        {
-            base.SpawnSetup(map, respawningAfterLoad);
-            mindState.lastJobTag = JobTag.Idle;
-            guest = new Pawn_GuestTracker(this);
-            if (drafter == null && this.TryGetComp<CompMachine>().Props.violent)
-            {
-                drafter = new Pawn_DraftController(this);
-            }
-
-            if (base.Faction == Faction.OfPlayer && base.Name == null)
-            {
-                base.Name = PawnBioAndNameGenerator.GeneratePawnName(this, NameStyle.Numeric);
-            }
-        }
-
-        public override void PostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
-        {
-            base.PostApplyDamage(dinfo, totalDamageDealt);
-            if (!health.Dead && (!health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) || !health.capacities.CapableOf(PawnCapacityDefOf.Moving)))
-            {
-                Kill(dinfo);
-            }
-        }
     }
 
     //一般情況的
