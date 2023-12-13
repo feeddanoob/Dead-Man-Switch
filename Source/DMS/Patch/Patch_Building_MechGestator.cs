ca7265bc33cce119a0a8a18cee72e9b0288bf921
Aoba <45892714+AobaKuma@users.noreply.github.com>
2023-12-13 21:41:12 +0800

    更新V0.95B
    由玩家所製造的可替換武器機兵現在不會初始自帶武器了
    玩家方的機兵倒地後武器不會消失了
    四軸無人機現在可以裝備CE的無線電了

---
+++ Source/DMS/Patch/Patch_Building_MechGestator.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DMS
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(GenRecipe), "PostProcessProduct")]
+    static class Patch_PostProcessProduct
+    {
+        [HarmonyPrefix]
+        static bool PreFix(Thing product)
+        {
+            if (product is WeaponUsableMech pawn)
+            {
+                pawn.inventory.DestroyAll();
+                pawn.equipment.DestroyAllEquipment();
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/DMS/Patch/Patch_Pawn.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DMS
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(Pawn), "DropAndForbidEverything")]
+    static class Patch_Pawn
+    {
+        [HarmonyPrefix]
+        static bool PreFix(Pawn __instance)
+        {
+            if (__instance is WeaponUsableMech pawn && pawn.Faction != Faction.OfPlayer)
+            {
+                __instance.equipment.DestroyAllEquipment();
+                __instance.apparel.DestroyAll();
+            }
+            return true;
+        }
+    }
+}
