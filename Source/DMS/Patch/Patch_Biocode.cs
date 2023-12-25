eebe194c337cdb60fb52638fd152e19b2c7073ca
Aoba <45892714+AobaKuma@users.noreply.github.com>
2023-12-25 18:34:23 +0800

    更新1.02
    修復了官階非正常生成的問題
    修復了呼叫支援無法到達的問題
    修復了錯誤了權限繼承問題
    修復了工程駁機無法復活的問題
    修復了多餘的statDef
    將集群接收器的製造材料更改為TR組件。

---
+++ Source/DMS/Patch/Patch_Biocode.cs
@@ -0,0 +1,23 @@
+﻿using Verse;
+using RimWorld;
+using System.Reflection;
+using HarmonyLib;
+
+namespace DMS
+{
+    [HarmonyPatch(typeof(CompBiocodable), "CodeFor")]
+    internal static class Patch_Biocode
+    {
+        public static bool Prefix(Pawn p, CompBiocodable __instance)
+        {
+            if (p.Name == null && __instance.Biocodable)
+            {
+                typeof(CompBiocodable).GetField("biocoded", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(__instance, true);
+                typeof(CompBiocodable).GetField("codedPawn", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(__instance, p);
+                typeof(CompBiocodable).GetField("codedPawnLabel", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(__instance, p.Label);
+                return false;
+            }
+            return true;
+        }
+    }
+}
