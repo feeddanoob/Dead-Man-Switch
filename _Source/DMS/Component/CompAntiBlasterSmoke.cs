071c7ebd1036a029cf38e619e5212a7ba07a32a9
Aoba <45892714+AobaKuma@users.noreply.github.com>
2024-03-17 23:55:09 +0800

    1.4 final

---
+++ _Source/DMS/Component/CompAntiBlasterSmoke.cs
@@ -0,0 +1,147 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using System.Reflection;
+using UnityEngine;
+using System;
+
+namespace DMS
+{
+    public class CompAntiBlasterSmoke : ThingComp
+    {
+        [Unsaved(false)]
+        private Effecter effecter;
+        private CompProperties_AntiBlasterSmoke Props => (CompProperties_AntiBlasterSmoke)props;
+        private bool isActive = true;
+        private int tickRemain = 100;
+        private Thing EffecterSourceThing
+        {
+            get
+            {
+                ThingWithComps pawn = parent;
+                if (!parent.Spawned)
+                {
+                    IThingHolder parentHolder = parent.ParentHolder;
+                    if (parentHolder != null)
+                    {
+                        if (parentHolder is Pawn_ApparelTracker pawn_ApparelTracker)
+                        {
+                            pawn = pawn_ApparelTracker.pawn;
+                        }
+                        else if (parentHolder is Pawn_CarryTracker pawn_CarryTracker)
+                        {
+                            pawn = pawn_CarryTracker.pawn;
+                        }
+                        else if (parentHolder is Pawn_EquipmentTracker pawn_EquipmentTracker)
+                        {
+                            pawn = pawn_EquipmentTracker.pawn;
+                        }
+                    }
+                }
+
+                return pawn;
+            }
+        }
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            effecter?.Cleanup();
+            effecter = null;
+        }
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            tickRemain = Props.activeTicks;
+        }
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (!isActive) return;
+            if (Props.effecterDef != null)
+            {
+                if (effecter == null)
+                {
+                    effecter = Props.effecterDef.Spawn();
+                }
+
+                effecter.EffectTick(EffecterSourceThing, TargetInfo.Invalid);
+            }
+
+            foreach (IntVec3 item in GenAdj.OccupiedRect(parent).ExpandedBy(Props.Size))
+            {
+                List<Thing> list = parent.MapHeld.thingGrid.ThingsListAt(item).Where((v) => v is Projectile).ToList();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Thing thing2 = list[i];
+                    if (IsTargetProjectile(thing2) && Vector3.Distance(thing2.DrawPos, parent.DrawPos) < Props.Size)
+                    {
+                        if (Rand.Range(0f, 1f) > Props.chanceToFail)
+                            DoIntercept(thing2 as Projectile);
+                    }
+                }
+            }
+            tickRemain--;
+            if (tickRemain <= 0) isActive = false;
+        }
+        private void DoIntercept(Projectile target)
+        {
+            if (target == null) return;
+            if (Props.fleckDef != null)
+            {
+                if (parent.DrawPos.ShouldSpawnMotesAt(parent.Map, false))
+                {
+                    FleckCreationData dataStatic = FleckMaker.GetDataStatic(target.DrawPos, parent.Map, Props.fleckDef, Rand.Range(0.5f, 1.5f));
+                    dataStatic.rotation = target.Rotation.AsAngle;
+                    dataStatic.targetSize = 0;
+                    dataStatic.velocityAngle = target.Rotation.AsAngle;
+                    dataStatic.velocity = Vector3.right;
+                    dataStatic.velocitySpeed = Rand.Range(target.def.projectile.speed / 2, target.def.projectile.speed);
+                    parent.Map.flecks.CreateFleck(dataStatic);
+                }
+            }
+            if (Props.spawnLeaving != null)
+            {
+                if (Rand.Range(0f, 1f) > 0.8f)
+                    GenSpawn.Spawn(Props.spawnLeaving, target.Position, target.Map);
+            }
+            target.Destroy();
+        }
+        private bool IsTargetProjectile(Thing target)
+        {
+            if (target is null) return false;
+
+            if (target is Projectile)
+            {
+                if (target.def.defName.Contains("Charge") || target.def.defName.Contains("Blaster"))
+                {
+                    if (Props.ignoreThings.Contains(target.def.defName)) return false;
+                    return true;
+                }
+                if (!Props.interceptThings.Where((v => target.def.defName == v)).FirstOrDefault().NullOrEmpty()) return true;
+            }
+            return false;
+        }
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref isActive, "isActive", true);
+            Scribe_Values.Look(ref tickRemain, "tickRemain", 100); 
+        }
+    }
+    public class CompProperties_AntiBlasterSmoke : CompProperties
+    {
+        public int Size;
+        public EffecterDef effecterDef;
+
+        public FleckDef fleckDef;
+        public ThingDef spawnLeaving;
+        public float chanceToFail = 0.8f;
+        public int activeTicks = 1500;
+        public List<string> interceptThings;
+        public List<string> ignoreThings;
+        public CompProperties_AntiBlasterSmoke()
+        {
+            compClass = typeof(CompAntiBlasterSmoke);
+        }
+    }
+}
