7a4733e53389a4c40f4063c11c097ba31679db55
Aoba <sakilomo@gmail.com>
2024-07-31 06:31:01 +0800

    update 1.49A

--- _Source/DMS/Modification/ModificationUtility.cs
+++ _Source/DMS/Modification/ModificationUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RimWorld;
+using System.Collections.Generic;
 using System.Linq;
 using Verse;
 
@@ -35,15 +36,18 @@ namespace DMS
             }
             if (comp.targetBodyPartDefs.NullOrEmpty()) return true;//如果是空的那就是裝全身的
 
+            //理論上可以安裝的部位
             List<BodyPartRecord> bodyParts = pawn.RaceProps.body.AllParts.Where(p => comp.targetBodyPartDefs.Contains(p.def)).ToList();
+            if (bodyParts.NullOrEmpty()) return false;
 
-            foreach (var hediff in pawn.health.hediffSet.hediffs.Where(h => comp.targetBodyPartDefs.Contains(h.Part.def)))
-            {//透過hediff默認安裝位置來找對應hediff,然後從所有零件位置中去除有安裝的部位,不確定有沒有更有效率的方式。
+            foreach (Hediff hediff in pawn.health.hediffSet.hediffs.Where(h => comp.targetBodyPartDefs.Contains(h.Part.def)))
+            {//然後從所有零件位置中去除有安裝的部位,不確定有沒有更有效率的方式。
                 bodyParts.Remove(hediff.Part);
             }
             if (bodyParts.NullOrEmpty()) return false;
+
+            //如果還有地方那就裝在這裡了。
             bodyPart = bodyParts.FirstOrDefault();
-            //如果存在沒有安裝仿生體的部位，那就返回true,以及默認第一個。
             return true;
         }
         public static bool HasAnyBodyPartOf(Pawn pawn, List<BodyPartDef> partDefs)
--- _Source/DMS/MultiTurrets/PawnRenderNodeWorker_SubTurretGun.cs
+++ _Source/DMS/MultiTurrets/PawnRenderNodeWorker_SubTurretGun.cs
@@ -24,4 +24,5 @@ namespace DMS
 			return quaternion;
 		}
 	}
+
 }
