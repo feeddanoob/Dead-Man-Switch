94876245df09abd69107534861667849d0a10a17
Aoba <sakilomo@gmail.com>
2024-08-06 10:06:35 +0800

    Version 1.50A
    added neuromech bionic for internal organ such like lung and kidney.
    added combat ration for supply need
    added texture for neurohord

---
+++ _Source/DMS/DMS_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+
+namespace DMS
+{
+    [RimWorld.DefOf]
+    public static class DMS_DefOf
+    {
+        public static ThoughtDef DMS_OverEat;
+    }
+
+}
---
+++ _Source/DMS/Hediff/IngestionOutcomeDoer_GiveHediffIfNoBionic.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace DMS
+{
+    public class IngestionOutcomeDoer_GiveThoughtWhenNoBionic : IngestionOutcomeDoer
+    {
+        public HediffDef BionicHediff;
+
+        public ThoughtDef thoughtDef;
+
+        public float severity = -1f;
+
+        public ChemicalDef toleranceChemical;
+
+        private bool divideByBodySize;
+
+        public bool multiplyByGeneToleranceFactors;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            //沒有特定Hediff才會產生副作用
+            if (!pawn.health.hediffSet.HasHediff(BionicHediff))
+            {
+                pawn.needs.mood.thoughts.memories.TryGainMemory(DMS_DefOf.DMS_OverEat, null, null);
+            }
+        }
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+        {
+            if (!parentDef.IsDrug || !(chance >= 1f))
+            {
+                yield break;
+            }
+            foreach (StatDrawEntry item in thoughtDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+            {
+                yield return item;
+            }
+        }
+    }
+}
