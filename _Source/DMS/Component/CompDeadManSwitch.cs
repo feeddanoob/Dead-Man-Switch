5a9fa3708550b42f74146530e07bebfb918c5dd1
Aoba <sakilomo@gmail.com>
2024-11-14 23:25:33 +0800

    update 11/14

--- _Source/DMS/Component/CompDeadManSwitch.cs
+++ _Source/DMS/Component/CompDeadManSwitch.cs
@@ -22,7 +22,7 @@ namespace DMS
             }
             if (delayCheck <= 0)
             {
-                TryTriggerDMS();
+                //TryTriggerDMS();
                 delayCheck = Props.minDelayUntilDMS;
             }
         }
