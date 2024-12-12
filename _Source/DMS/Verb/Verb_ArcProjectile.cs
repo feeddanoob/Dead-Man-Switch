5afa74f6c3d9ed99b37ff8a1bbcbb1cfaacee06e
Aoba <sakilomo@gmail.com>
2024-12-12 21:04:39 +0800

    upd

--- _Source/DMS/Verb/Verb_ArcProjectile.cs
+++ _Source/DMS/Verb/Verb_ArcProjectile.cs
@@ -1,5 +1,6 @@
 ﻿using Mono.Unix.Native;
 using RimWorld;
+using RimWorld.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,146 +12,116 @@ using static HarmonyLib.Code;
 
 namespace DMS
 {
+    public class Verb_CastAbilityArcSprayProjectile : Verb_CastAbility
+    {
+        protected List<IntVec3> path = new List<IntVec3>();
+
+        protected Vector3 initialTargetPosition;
+
+        protected override int ShotsPerBurst => verbProps.burstShotCount;
+
+        public override float? AimAngleOverride
+        {
+            get
+            {
+                if (state == VerbState.Bursting && Available())
+                {
+                    return (path[ShotsPerBurst - burstShotsLeft].ToVector3Shifted() - caster.DrawPos).AngleFlat();
+                }
+                return null;
+            }
+        }
+        protected override bool TryCastShot()
+        {
+            if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+            {
+                return false;
+            }
+            ShootLine resultingLine;
+            bool flag = TryFindShootLineFromTo(caster.Position, currentTarget, out resultingLine);
+            if (verbProps.stopBurstWithoutLos && !flag)
+            {
+                return false;
+            }
+            if (base.EquipmentSource != null && burstShotsLeft <= 1)
+            {
+                base.EquipmentSource.GetComp<CompChangeableProjectile>()?.Notify_ProjectileLaunched();
+                base.EquipmentSource.GetComp<CompApparelReloadable>()?.UsedOnce();
+            }
+            HitCell(path[ShotsPerBurst - burstShotsLeft]);
+            lastShotTick = Find.TickManager.TicksGame;
+            return true;
+        }
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            base.ValidateTarget(target, showMessages);
+            if (!ReloadableUtility.CanUseConsideringQueuedJobs(CasterPawn, base.EquipmentSource))
+            {
+                return false;
+            }
+            return true;
+        }
+        public override bool Available()
+        {
+            return ShotsPerBurst - burstShotsLeft >= 0;
+        }
+
+        public override void WarmupComplete()
+        {
+            burstShotsLeft = ShotsPerBurst;
+            state = VerbState.Bursting;
+            initialTargetPosition = currentTarget.CenterVector3;
+            PreparePath();
+            TryCastNextBurstShot();
+        }
+        protected void PreparePath()
+        {
+            path.Clear();
+            Vector3 normalized = (currentTarget.CenterVector3 - caster.Position.ToVector3Shifted()).Yto0().normalized;
+            Vector3 tan = normalized.RotatedBy(90f);
+            for (int i = 0; i < verbProps.sprayNumExtraCells; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    float value = Rand.Value;
+                    float num = Rand.Value - 0.5f;
+                    float num2 = value * verbProps.sprayWidth * 2f - verbProps.sprayWidth;
+                    float num3 = num * (float)verbProps.sprayThicknessCells + num * 2f * verbProps.sprayArching;
+                    IntVec3 item = (currentTarget.CenterVector3 + num2 * tan - num3 * normalized).ToIntVec3();
+                    if (!path.Contains(item) || Rand.Value < 0.25f)
+                    {
+                        path.Add(item);
+                        break;
+                    }
+                }
+            }
+            path.Add(currentTarget.Cell);
+            path.SortBy((IntVec3 c) => (c.ToVector3Shifted() - caster.DrawPos).Yto0().normalized.AngleToFlat(tan));
+        }
+
+        protected virtual void HitCell(IntVec3 cell)
+        {
+            verbProps.sprayEffecterDef?.Spawn(caster.Position, cell, caster.Map); 
+            ((Projectile)GenSpawn.Spawn(verbProps.defaultProjectile, caster.Position, caster.Map, WipeMode.Vanish)).Launch(caster, caster.DrawPos, cell, cell, ProjectileHitFlags.All, false, EquipmentSource?? null, null);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref path, "path", LookMode.Value);
+            Scribe_Values.Look(ref initialTargetPosition, "initialTargetPosition");
+            if (Scribe.mode == LoadSaveMode.PostLoadInit && path == null)
+            {
+                path = new List<IntVec3>();
+            }
+        }
+    }
     public class Verb_ArcSprayProjectile : Verb_ArcSpray
     {
         protected override void HitCell(IntVec3 cell)
         {
             base.HitCell(cell);
-
-            //LocalTargetInfo tar = cell;
-            //if (cell.TryGetFirstThing(map, out Thing t)) tar = t;
             ((Projectile)GenSpawn.Spawn(verbProps.defaultProjectile, caster.Position, caster.Map, WipeMode.Vanish)).Launch(caster, caster.DrawPos, cell, cell, ProjectileHitFlags.All, false, null, null);
-
         }
     }
-
-    public class CompProperties_AbilityStrafing : CompProperties_AbilityEffect
-    {
-        public float range;
-
-        public float lineWidthEnd;
-
-        public ThingDef projectileDef;
-
-        public EffecterDef effecterDef;
-
-        public bool canHitFilledCells;
-
-        public CompProperties_AbilityStrafing()
-        {
-            compClass = typeof(CompAbilityEffect_Strafing);
-        }
-    }
-
-    public class CompAbilityEffect_Strafing : CompAbilityEffect
-    {
-        private readonly List<IntVec3> tmpCells = new List<IntVec3>();
-
-        private new CompProperties_AbilityStrafing Props => (CompProperties_AbilityStrafing)props;
-
-        private Pawn Pawn => parent.pawn;
-        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
-        {
-            base.Apply(target, dest);
-            ((Projectile)GenSpawn.Spawn(Props.projectileDef, Pawn.Position, Pawn.Map, WipeMode.Vanish)).Launch(Pawn, Pawn.DrawPos, target, dest, ProjectileHitFlags.All, false, null, null);
-        }
-        public override IEnumerable<PreCastAction> GetPreCastActions()
-        {
-            if (Props.effecterDef != null)
-            {
-                yield return new PreCastAction
-                {
-                    action = delegate (LocalTargetInfo a, LocalTargetInfo b)
-                    {
-                        parent.AddEffecterToMaintain(Props.effecterDef.Spawn(parent.pawn.Position, a.Cell, parent.pawn.Map), Pawn.Position, a.Cell, 17, Pawn.MapHeld);
-                    },
-                    ticksAwayFromCast = 17
-                };
-            }
-        }
-
-        public override void DrawEffectPreview(LocalTargetInfo target)
-        {
-            GenDraw.DrawFieldEdges(AffectedCells(target));
-        }
-
-        public override bool AICanTargetNow(LocalTargetInfo target)
-        {
-            if (Pawn.Faction != null)
-            {
-                foreach (IntVec3 item in AffectedCells(target))
-                {
-                    List<Thing> thingList = item.GetThingList(Pawn.Map);
-                    for (int i = 0; i < thingList.Count; i++)
-                    {
-                        if (thingList[i].Faction == Pawn.Faction)
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-            return true;
-        }
-        private List<IntVec3> AffectedCells(LocalTargetInfo target)
-        {
-            tmpCells.Clear();
-            Vector3 vector = Pawn.Position.ToVector3Shifted().Yto0();
-            IntVec3 intVec = target.Cell.ClampInsideMap(Pawn.Map);
-            if (Pawn.Position == intVec)
-            {
-                return tmpCells;
-            }
-            float lengthHorizontal = (intVec - Pawn.Position).LengthHorizontal;
-            float num = (float)(intVec.x - Pawn.Position.x) / lengthHorizontal;
-            float num2 = (float)(intVec.z - Pawn.Position.z) / lengthHorizontal;
-            intVec.x = Mathf.RoundToInt((float)Pawn.Position.x + num * Props.range);
-            intVec.z = Mathf.RoundToInt((float)Pawn.Position.z + num2 * Props.range);
-            float target2 = Vector3.SignedAngle(intVec.ToVector3Shifted().Yto0() - vector, Vector3.right, Vector3.up);
-            float num3 = Props.lineWidthEnd / 2f;
-            float num4 = Mathf.Sqrt(Mathf.Pow((intVec - Pawn.Position).LengthHorizontal, 2f) + Mathf.Pow(num3, 2f));
-            float num5 = 57.29578f * Mathf.Asin(num3 / num4);
-            int num6 = GenRadial.NumCellsInRadius(Props.range);
-            for (int i = 0; i < num6; i++)
-            {
-                IntVec3 intVec2 = Pawn.Position + GenRadial.RadialPattern[i];
-                if (CanUseCell(intVec2) && Mathf.Abs(Mathf.DeltaAngle(Vector3.SignedAngle(intVec2.ToVector3Shifted().Yto0() - vector, Vector3.right, Vector3.up), target2)) <= num5)
-                {
-                    tmpCells.Add(intVec2);
-                }
-            }
-            List<IntVec3> list = GenSight.BresenhamCellsBetween(Pawn.Position, intVec);
-            for (int j = 0; j < list.Count; j++)
-            {
-                IntVec3 intVec3 = list[j];
-                if (!tmpCells.Contains(intVec3) && CanUseCell(intVec3))
-                {
-                    tmpCells.Add(intVec3);
-                }
-            }
-            return tmpCells;
-            bool CanUseCell(IntVec3 c)
-            {
-                if (!c.InBounds(Pawn.Map))
-                {
-                    return false;
-                }
-                if (c == Pawn.Position)
-                {
-                    return false;
-                }
-                if (!Props.canHitFilledCells && c.Filled(Pawn.Map))
-                {
-                    return false;
-                }
-                if (!c.InHorDistOf(Pawn.Position, Props.range))
-                {
-                    return false;
-                }
-                ShootLine resultingLine;
-                return parent.verb.TryFindShootLineFromTo(parent.pawn.Position, c, out resultingLine);
-            }
-        }
-    }
-
 }
