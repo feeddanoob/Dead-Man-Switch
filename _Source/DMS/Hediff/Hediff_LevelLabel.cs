aa43d9d0750e1ad90735e487ca031d5392bcf9d7
Aoba <sakilomo@gmail.com>
2025-02-21 07:08:53 +0800

    聲光效果調整與更新
    - 添加了屬於Lady的武裝校準機制。
    - 優化了DMS砲塔的的聲效，並添加了MuzzleFlash支持。
    - 為大口徑武器新增了發射效果。
    - 修正了AI使用APS技能的時機與判斷優化。

---
+++ _Source/DMS/Hediff/Hediff_LevelLabel.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace DMS
+{
+    public class Hediff_LevelLabel : Hediff_Level
+    {
+        public override string Label
+        {
+            get
+            {
+                if (!def.levelIsQuantity)
+                {
+                    return def.label + " (" + def.stages[level].label + ")";
+                }
+                return def.label + " x" + level;
+            }
+        }
+    }
+}
