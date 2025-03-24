62c5d077bb32a3f92dc7345430095beca6de9bbc
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-03-24 12:10:12 +0000

    init commit

---
+++ _Source/DMS/AirSupportData.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace DMS
+{
+    public abstract class AirSupportData : IExposable
+    {
+        public Map map;
+
+        public IntVec3 targetCell;
+
+        public int triggerTick;
+
+        public Thing triggerer;
+
+        public Faction triggerFaction;
+
+        public virtual void ExposeData()
+        {
+            Scribe_References.Look(ref map, "map");
+            Scribe_References.Look(ref triggerer, "triggerer");
+            Scribe_References.Look(ref triggerFaction, "triggerFaction");
+            Scribe_Values.Look(ref targetCell, "cell");
+            Scribe_Values.Look(ref triggerTick, "triggerTick");
+        }
+
+        public abstract void Trigger();
+    }
+
+    public class AirSupportData_LaunchProjectile : AirSupportData
+    {
+        public ThingDef projectileDef;
+        public Vector3 origin;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref projectileDef, "projectileDef");
+            Scribe_Values.Look(ref origin, "origin");
+        }
+
+        public override void Trigger()
+        {
+            Projectile projectile = (Projectile)GenSpawn.Spawn(projectileDef, origin.ToIntVec3(), map);
+            projectile.SetFactionDirect(triggerer?.Faction ?? triggerFaction);
+            projectile.Launch(triggerer, origin, targetCell, targetCell, ProjectileHitFlags.IntendedTarget);
+        }
+    }
+}
---
+++ _Source/DMS/CompAirSupportSummoner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace DMS
+{
+    public class CompAirSupportSummoner : ThingComp
+    {
+        CompProperties_AirSupportSummoner Props => props as CompProperties_AirSupportSummoner;
+        public override IEnumerable<Gizmo> CompGetWornGizmosExtra()
+        {
+            yield return new Command_Action
+            {
+                action = new Action(DoEffect),
+                defaultDesc = parent.def.description,
+                icon = parent.def.uiIcon,
+                defaultLabel = "boom",
+            };
+        }
+
+        public void DoEffect()
+        {
+            int delay = Find.TickManager.TicksGame + Props.delayRange.RandomInRange;
+            var c = GenRadial.NumCellsInRadius(Props.spreadRadius);
+            var ori = CellFinder.RandomEdgeCell(parent.MapHeld).ToVector3Shifted();
+
+            Thing triggerer = parent.ParentHolder is Pawn pawn ? pawn : parent;
+
+            for (int i = 0; i < Props.burstCount; i++)
+            {
+                GameComponent_CAS.AddData(new AirSupportData_LaunchProjectile()
+                {
+                    projectileDef = Props.ProjectileDef,
+                    map = parent.MapHeld,
+                    targetCell = GenRadial.RadialPattern[Rand.RangeInclusive(0, c)],
+                    triggerTick = delay,
+                    triggerer = triggerer,
+                    triggerFaction = triggerer.Faction,
+                    origin = ori
+                });
+                delay += Props.burstInterval;
+            }
+        }
+    }
+
+    public class CompProperties_AirSupportSummoner : CompProperties
+    {
+        public CompProperties_AirSupportSummoner()
+        {
+            compClass = typeof(CompAirSupportSummoner);
+        }
+
+        public ThingDef ProjectileDef;
+
+        public float spreadRadius = 5;
+
+        public int burstCount = 1, burstInterval = 5;
+
+        public IntRange delayRange = new IntRange(120, 150);
+    }
+}
