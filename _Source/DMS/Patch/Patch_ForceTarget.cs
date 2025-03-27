961ac748edfdb4b0aa1c8abf509957a0a6018687
Aoba <sakilomo@gmail.com>
2025-03-28 02:22:28 +0800

    upd

--- _Source/DMS/Patch/Patch_ForceTarget.cs
+++ _Source/DMS/Patch/Patch_ForceTarget.cs
@@ -1,16 +1,15 @@
 ﻿using Verse;
 using RimWorld;
-
 using HarmonyLib;
 
 namespace DMS
 {
-    [HarmonyPatch(typeof(Building_TurretGun), "CanSetForcedTarget",  (MethodType)1)]
+    [HarmonyPatch(typeof(Building_TurretGun), "CanSetForcedTarget", (MethodType)1)]
     internal static class Patch_ForceTarget
     {
         public static void Postfix(ref bool __result, Building_TurretGun __instance)
         {
-            if (__instance.Faction.IsPlayer && ThingCompUtility.TryGetComp<CompForceTargetable>(__instance) != null)
+            if (ThingCompUtility.TryGetComp<CompForceTargetable>(__instance) != null && __instance.Faction != null && __instance.Faction.IsPlayer)
             {
                 __result = true;
             }
