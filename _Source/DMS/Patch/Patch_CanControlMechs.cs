4e39f92ff4bcfc465fd0ea5fa4ae6ee8910bba98
Aoba <sakilomo@gmail.com>
2025-04-14 10:31:13 +0800

    理論上修復了機器人訪客的問題。

--- _Source/DMS/Patch/Patch_CanControlMechs.cs
+++ _Source/DMS/Patch/Patch_CanControlMechs.cs
@@ -5,12 +5,13 @@ using System.Linq;
 
 namespace DMS
 {
-    [HarmonyPatch(typeof(Pawn_MechanitorTracker), nameof(Pawn_MechanitorTracker.CanControlMechs),MethodType.Getter)]
+    [HarmonyPatch(typeof(Pawn_MechanitorTracker), nameof(Pawn_MechanitorTracker.CanControlMechs), MethodType.Getter)]
     internal static class Patch_CanControlMechs
     {
         internal static void Postfix(Pawn_MechanitorTracker __instance, ref AcceptanceReport __result)
         {
             if (__result == true) return;
+            if (__instance.Pawn.HostFaction != null) __result = true;
             if (__instance.OverseenPawns?.Where(p => p.TryGetComp<CompCommandRelay>() != null)?.Count() > 0) __result = true;
         }
     }
